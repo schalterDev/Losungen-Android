00e02398cd3c9a739c6d336d8dd48e9a7e323462
SchalterDev <schalter.dev@gmail.com>
2017-10-19 11:54:25 +0200

    Updated import settings to support 2018

--- app/src/main/java/de/schalter/losungen/settings/Tags.java
+++ app/src/main/java/de/schalter/losungen/settings/Tags.java
@@ -16,7 +16,7 @@ public class Tags {
 
     //Für Losungen, Wochen und Monatssprüche
     private static final String[] IMPORT_LIST_DE = {"2017", "2018"};
-    private static final String[] IMPORT_LIST_EN = {"2015", "2016", "2017"};
+    private static final String[] IMPORT_LIST_EN = {"2016", "2017", "2018"};
     private static final String[] IMPORT_LIST_ES = {"2015", "2016"};
     private static final String[] IMPORT_LIST_AR = {};
     private static final String[] IMPORT_LIST_FR = {"2015", "2016"};
