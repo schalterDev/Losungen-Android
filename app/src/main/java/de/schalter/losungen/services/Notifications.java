ac0199de0a932693ffd18481138cbb0b6f803ac4
Martin Schalter <schalter.dev@gmail.com>
2018-11-09 17:43:04 +0100

    :zap: show notification when notifications are activated in the settings

--- app/src/main/java/de/schalter/losungen/services/Notifications.java
+++ app/src/main/java/de/schalter/losungen/services/Notifications.java
@@ -58,17 +58,6 @@ public class Notifications extends Service {
     public static void setNotifications(Context context, long time) {
         CustomLog.writeToLog(context, new CustomLog(CustomLog.DEBUG, CustomLog.TAG_NOTIFICATION, "Set Notification with time: " + time));
 
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
-        long lastNotification = settings.getLong(Tags.TAG_LASTNOTIFICATION, 0);
-
-        //Was this day allready a notification?
-        Calendar calendarNotification = Calendar.getInstance();
-        calendarNotification.setTimeInMillis(lastNotification);
-        boolean notificationToday = (calendarNotification.get(Calendar.DAY_OF_YEAR) ==
-                Calendar.getInstance().get(Calendar.DAY_OF_YEAR));
-
-        CustomLog.writeToLog(context, new CustomLog(CustomLog.DEBUG, CustomLog.TAG_NOTIFICATION, "Was this day already a notification?: " + notificationToday));
-
         Intent intent = new Intent(context, Notifications.class);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -80,8 +69,6 @@ public class Notifications extends Service {
         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
         calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, 0);
-        //If their was a notification today add one day
-        if(notificationToday) calendar.add(Calendar.DAY_OF_YEAR, 1);
 
         alarmManager.cancel(pendingIntent);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 24 * 60 * 60 * 1000, pendingIntent);
@@ -98,7 +85,7 @@ public class Notifications extends Service {
         alarmManager.cancel(pendingIntent);
 
         CustomLog.writeToLog(context, new CustomLog(CustomLog.DEBUG, CustomLog.TAG_NOTIFICATION, "Notifications canceled"));
-        Log.i("Losungen", "Notifications canceled");
+        Log.i("Losungen", "Notifications removed");
     }
 
     private void showNotification(Context context, String titel, String msg, long datum) {
@@ -168,6 +155,11 @@ public class Notifications extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        CustomLog.writeToLog(this, new CustomLog(
+                CustomLog.DEBUG,
+                CustomLog.TAG_NOTIFICATION,
+                "Started service with action: " + intent.getAction()
+        ));
 
         Context context = getApplicationContext();
 
