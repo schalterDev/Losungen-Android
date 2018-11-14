901ef77d797629c98258782c1141c849feb8c1a4
Martin Schalter <schalter.dev@gmail.com>
2018-11-14 17:49:07 +0100

    Revert ":racehorse: move notification again to service "
    This reverts commit b3c87aafc36cff0fb8cb4bc4a1085f523556b136.

--- app/src/main/java/de/schalter/losungen/services/AlarmReceiver.java
+++ app/src/main/java/de/schalter/losungen/services/AlarmReceiver.java
@@ -4,12 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 import de.schalter.losungen.Losung;
+import de.schalter.losungen.MainActivity;
 import de.schalter.losungen.R;
 import de.schalter.losungen.files.DBHandler;
 import de.schalter.losungen.log.CustomLog;
@@ -20,8 +21,19 @@ public class AlarmReceiver extends BroadcastReceiver {
     public static final String SHOW_NOTIFICATION = "de.schalter.losungen.show_notificaiton";
     public static final String DOWNLOAD_AUDIO = "de.scahlter.losungen.download_audio";
 
+    public static final String NOTIFICATION_SHARE = "SHARE";
+    public static final String NOTIFICATION_SHARE_MESSAGE = "message";
+    public static final String NOTIFICATION_SHARE_TITLE = "title";
+
+    public static final String NOTIFICATION_MARK = "MARK";
+    public static final String NOTIFICATION_MARK_DATE = "date";
+
+    private Context context;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        this.context = context;
+
         String action = intent.getAction();
 
         CustomLog.writeToLog(
@@ -36,14 +48,49 @@ public class AlarmReceiver extends BroadcastReceiver {
         if (action != null) {
             switch (action) {
                 case SHOW_NOTIFICATION:
-                    Intent intentShowNotification = new Intent(context.getApplicationContext(), Notifications.class);
-                    intentShowNotification.setAction(Notifications.NOTIFICATION_SHOW);
-                    context.startService(intentShowNotification);
+                    showNotificationInitial();
                     break;
                 case DOWNLOAD_AUDIO:
-                    Intent intentService = new Intent(context.getApplicationContext(), AudioDownloadService.class);
+                    Intent intentService = new Intent(context, AudioDownloadService.class);
                     context.startService(intentService);
                     break;
+            }
+        }
+    }
+
+    private void showNotificationInitial() {
+        DBHandler dbHandler = DBHandler.newInstance(context);
+        Calendar calendar = Calendar.getInstance();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean showNotification = settings.getBoolean(Tags.PREF_NOTIFICATION, true);
+
+        //Show Notification
+        if (showNotification) {
+            Losung losung = dbHandler.getLosung(calendar.getTimeInMillis());
+
+            if (!losung.getLosungstext().equals(context.getResources().getString(R.string.no_date))) {
+                switch (settings.getInt(Tags.PREF_NOTIFICATIONART, Tags.LOSUNG_UND_LEHRTEXT_NOTIFICATION)) {
+                    case Tags.LOSUNG_NOTIFICATION:
+                        Notifications.showNotification(context, context.getResources().getString(R.string.losung),
+                                losung.getLosungstext() + System.getProperty("line.separator") + losung.getLosungsvers(),
+                                losung.getDate());
+                        break;
+
+                    case Tags.LEHRTEXT_NOTIFICATION:
+                        Notifications.showNotification(context, context.getResources().getString(R.string.lehrtext),
+                                losung.getLehrtext() + System.getProperty("line.separator") + losung.getLehrtextVers(),
+                                losung.getDate());
+                        break;
+
+                    case Tags.LOSUNG_UND_LEHRTEXT_NOTIFICATION:
+                        Notifications.showNotification(context, context.getResources().getString(R.string.losungen),
+                                losung.getLosungstext() + System.getProperty("line.separator") + losung.getLosungsvers() +
+                                        System.getProperty("line.separator") + System.getProperty("line.separator") +
+                                        losung.getLehrtext() + System.getProperty("line.separator") + losung.getLehrtextVers(),
+                                losung.getDate());
+                        break;
+                }
             }
         }
     }
